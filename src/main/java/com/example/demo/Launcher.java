89e468493f8230f050c14bcc3d87028d23624c99
Juan Carlos <jballestaalvarez@gmail.com>
2025-01-08 17:28:07 +0100

    Actualización del repositorio con proyecto Maven

---
+++ src/main/java/com/example/demo/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+	}
+
+}
---
+++ src/main/java/es/iesjandula/demo/App.java
@@ -0,0 +1,7 @@
+package es.iesjandula.demo;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Editado desde Eclipse");
+  }
+}
